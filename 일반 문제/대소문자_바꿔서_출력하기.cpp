1db577af0aaf2264eaffe32c95a0fac4aad11dc8
ncs01060 <ncs01045@gmail.com>
2026-06-03 17:08:16 +0900

    Day1. 대소문자_바꿔서_출력하기

---
+++ 일반 문제/대소문자_바꿔서_출력하기.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isupper(int* n){
+    if(*n >= 65 && *n <= 90){
+        return 1;
+    }
+    return 0;
+}
+
+bool islower(int* n){
+    if(*n >= 97 && *n <= 122){
+        return 1;
+    }
+    return 0;
+}
+
+int main(void) {
+    string str;
+    string result;
+    cin >> str;
+    for(int i = 0; i<str.length(); i++){
+        if(isupper(str[i])){
+            result += str[i] + 32;
+        }
+        else if(islower(str[i])){
+            result += str[i] - 32;
+            
+        }
+        else {
+            result += str[i];
+        }
+    
+    }
+    cout << result;
+    return 0;
+}
