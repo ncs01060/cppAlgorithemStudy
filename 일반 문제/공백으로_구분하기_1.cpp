f1041eb06bbc7b0b929afdc1deb6ff09ab88bd9b
ncs01060 <ncs01045@gmail.com>
2026-06-10 13:22:04 +0900

    공백으로_구분하기_1_notSolved

---
+++ 일반 문제/공백으로_구분하기_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    answer = my_string.
+    return answer;
+}
+
+int main(void){
+    cout << solution("Hello World")[0] << endl;
+    return 0;
+}
